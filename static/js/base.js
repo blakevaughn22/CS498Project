0df2dcb10c655d910387a2e1cdc16acb58004dcc
JohnMarshall <John.Marshall@uky.edu>
2023-04-07 10:38:22 -0400

    Separating html, css, and js code to separate files
    Separates html, css, and js code for easier future development and removes unnecessary/repeated code.  Also correctly implements sidebar functionality to all pages

---
+++ static/js/base.js
@@ -0,0 +1,7 @@
+function openNav(sidenavId) {
+  document.getElementById(sidenavId).style.width = "250px";
+}
+
+function closeNav(sidenavId) {
+  document.getElementById(sidenavId).style.width = "0";
+}
