29fb2823d93992a64049c75215fb7e0a025a1489
JohnMarshall <John.Marshall@uky.edu>
2023-04-26 18:02:50 -0400

    adding darkmode and improving ui
    added dark mode option and made logo files png files so that the background of images changes.

--- static/js/base.js
+++ static/js/base.js
@@ -5,3 +5,14 @@ function openNav(sidenavId) {
 function closeNav(sidenavId) {
   document.getElementById(sidenavId).style.width = "0";
 }
+
+function toggleDarkMode() {
+  const darkModeStyle = document.getElementById('dark-mode');
+  if (darkModeStyle.innerHTML) {
+    darkModeStyle.innerHTML = '';
+    localStorage.setItem('darkMode', 'disabled');
+  } else {
+    darkModeStyle.innerHTML = `body { background-color: rgb(34, 34, 34); color: white; }`;
+    localStorage.setItem('darkMode', 'enabled');
+  }
+}
